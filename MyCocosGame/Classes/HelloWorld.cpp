b66776021d65d3f567b61ec1fd6d649225f72950
student <student@MacBook-Pro-11.local>
2015-05-09 15:54:06 +0900

    0509

---
+++ MyCocosGame/Classes/HelloWorld.cpp
@@ -0,0 +1,9 @@
+//
+//  HelloWorld.cpp
+//  MyCocosGame
+//
+//  Created by student on 2015/05/09.
+//
+//
+
+#include "HelloWorld.h"
---
+++ MyCocosGame/Classes/HelloWorld.h
@@ -0,0 +1,14 @@
+//
+//  HelloWorld.h
+//  MyCocosGame
+//
+//  Created by student on 2015/05/09.
+//
+//
+
+#ifndef __MyCocosGame__HelloWorld__
+#define __MyCocosGame__HelloWorld__
+
+#include <stdio.h>
+
+#endif /* defined(__MyCocosGame__HelloWorld__) */
